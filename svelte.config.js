d93ab1f2a7011cb2b9a29cb1757832bd6a577ddd
CSK <73425927+cr2007@users.noreply.github.com>
2025-10-20 12:17:10 +0000

    feat: adds github pages support

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,19 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
 		}),
 		csp: {
 			directives: {
 				'script-src': ['self']
 			}
+		},
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: vitePreprocess()
