ed86648177779558c6df5d20d1ea5a531b31a309
CSK <73425927+cr2007@users.noreply.github.com>
2025-10-20 14:34:42 +0000

    fix: add VITE_DEPLOY_TARGET for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	base: '/omschecklist/'
+export default defineConfig(({ command, mode }) => {
+	const isGitHubPages = process.env.VITE_DEPLOY_TARGET === 'github-pages';
+
+	return {
+		plugins: [sveltekit()],
+		base: isGitHubPages ? process.env.BASE_PATH : '/',
+	};
 });
