6969311b4054d417e8efd031c5400100cf3d8b40
CSK <73425927+cr2007@users.noreply.github.com>
2025-10-20 14:36:51 +0000

    fix: update base path logic for GitHub Pages deployment

--- svelte.config.js
+++ svelte.config.js
@@ -17,7 +17,7 @@ const config = {
 			}
 		},
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.VITE_DEPLOY_TARGET === 'github-pages' ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: vitePreprocess()
